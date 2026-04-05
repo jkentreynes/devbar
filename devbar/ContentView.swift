61b7981eca18ef85c0af11835fd49af1687f607c
John Kent Reynes <johnkentreynes@gmail.com>
2026-04-05 12:44:46 +0800

    Initial Commit

---
+++ devbar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  devbar
+//
+//  Created by John Kent Reynes on 4/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ devbar/devbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  devbarApp.swift
+//  devbar
+//
+//  Created by John Kent Reynes on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct devbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
